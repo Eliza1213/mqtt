27b1855945248818b083a338d2d1618719c9e856
Eliza1213 <20230015@uthh.edu.mx>
2025-03-26 12:34:08 -0600

    Inicializando nuevo repositorio

--- server.js
+++ server.js
@@ -18,12 +18,12 @@ conectarDB();
 // Rutas API existentes
 app.use("/api/usuarios", require("./routes/userRoutes"));
 app.use("/api/misiones", require("./routes/MisionRoutes"));
-app.use("/api/visiones", require("./Routes/VisionRoutes"));
-app.use("/api/terminos", require("./Routes/TerminoRoutes"));
-app.use("/api/politicas", require("./Routes/PoliticaRoutes"));
-app.use("/api/preguntas", require("./Routes/PreguntaRoutes"));
-app.use("/api/contactos", require("./Routes/ContactoRoutes"));
-app.use("/api/informaciones", require("./Routes/InformacionRoutes"));
+app.use("/api/visiones", require("./routes/VisionRoutes"));
+app.use("/api/terminos", require("./routes/TerminoRoutes"));
+app.use("/api/politicas", require("./routes/PoliticaRoutes"));
+app.use("/api/preguntas", require("./routes/PreguntaRoutes"));
+app.use("/api/contactos", require("./routes/ContactoRoutes"));
+app.use("/api/informaciones", require("./routes/InformacionRoutes"));
 app.use("/api/productos", require("./routes/ProductoRoutes"));
 
 // Exportar la aplicación
